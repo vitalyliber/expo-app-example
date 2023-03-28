165af4c674563a22e333a74ba8a5a6b2bfa86150
vitaly liber <zenamax@gmail.com>
2023-03-28 11:33:31 +0300

    Keep the splash screen visible while we fetch resources

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,15 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
 
 import RootNavigator from "./RootNavigator";
 import { StatusBar } from "expo-status-bar";
 import TabNavigator from "./TabNavigator";
 import { useStore } from "@nanostores/react";
 import { user } from "../store/user";
+import useSplashScreenHook from "./useSplashScreenHook";
 
 export default function Navigation() {
+  const { onLayoutRootView, appIsReady } = useSplashScreenHook();
   const { authorized } = useStore(user);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
+      <View onLayout={onLayoutRootView} />
       <StatusBar style="auto" />
       {authorized ? <TabNavigator /> : <RootNavigator />}
     </NavigationContainer>
